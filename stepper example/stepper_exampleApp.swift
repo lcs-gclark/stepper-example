85ebce30a0755e2776062f2c71d70937781333f6
grayden clark <gclark@lcs.on.ca>
2024-01-15 11:04:22 -0500

    Initial Commit

---
+++ stepper example/stepper_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepper_exampleApp.swift
+//  stepper example
+//
+//  Created by Gray Clark on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct stepper_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
